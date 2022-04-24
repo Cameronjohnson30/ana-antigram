679f53197ffb36504fa2761c86779c75de0cb6cc
cameronjohnson30 <cameron_johnson91@icloud.com>
2022-04-23 18:39:45 -0700

    made 3 more tests for is_word function

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -7,4 +7,13 @@ describe("Words#is-word") do
   it("returns true if both word1 and word2 contain a vowel") do
     expect(words.is_word("hello", "yes")).to(eq(true))
   end
+  it("returns false if word2 does not contain vowel") do
+    expect(words.is_word("hello", "whp")).to(eq(false))
+end
+  it("returns false if word1 does not contain vowel") do
+    expect(words.is_word("whp", "hello")).to(eq(false))
+  end
+  it("returns false if both word1 or word2 does not contain vowel") do
+    expect(words.is_word("whp", "whp")).to(eq(false))
+  end
 end
