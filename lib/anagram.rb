28712d2f2d7b89532b4e092a1123e81a884a0733
cameronjohnson30 <cameron_johnson91@icloud.com>
2022-04-23 20:41:39 -0700

    changed lines 4,6,12,14 to make it user friendly

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,9 +1,9 @@
 class Words
   def is_word(word1, word2)
     if word1.scan(/[aeiou]/).count >= 1 && word2.scan(/[aeiou]/).count >= 1
-      true
+      is_anagram(word1,word2)
     else word1.scan(/[aeiou]/).count >= 1 && word2.scan(/[aeiou]/).count >= 0 || word1.scan(/[aeiou]/).count >= 0 && word2.scan(/[aeiou]/).count >= 1 || word1.scan(/[aeiou]/).count >= 0 && word2.scan(/[aeiou]/).count >= 0
-      false
+      p "please check your spelling" 
     end
   end
 
@@ -11,7 +11,7 @@ class Words
     if word1.downcase.chars.sort.join.gsub(/[^a-z ]/, '') == word2.downcase.chars.sort.join.gsub(/[^a-z ]/, '')
       p "These words are anagrams."
     else
-      false
+      is_antigram(word1, word2)
     end
   end
 
