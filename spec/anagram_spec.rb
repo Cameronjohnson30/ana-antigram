1f5d1a9ddd16e2d1e68343b1bb255d8544914bbd
cameronjohnson30 <cameron_johnson91@icloud.com>
2022-04-23 20:42:25 -0700

    changed the tests to new responses

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -5,16 +5,16 @@ words = Words.new()
 
 describe("Words#is-word") do
   it("returns true if both word1 and word2 contain a vowel") do
-    expect(words.is_word("hello", "yes")).to(eq(true))
+    expect(words.is_word("hello", "yes")).to(eq("These words are neither an anagram or antigram"))
   end
   it("returns false if word2 does not contain vowel") do
-    expect(words.is_word("hello", "whp")).to(eq(false))
+    expect(words.is_word("hello", "whp")).to(eq("please check your spelling"))
 end
   it("returns false if word1 does not contain vowel") do
-    expect(words.is_word("whp", "hello")).to(eq(false))
+    expect(words.is_word("whp", "hello")).to(eq("please check your spelling"))
   end
   it("returns false if both word1 or word2 does not contain vowel") do
-    expect(words.is_word("whp", "whp")).to(eq(false))
+    expect(words.is_word("whp", "whp")).to(eq("please check your spelling"))
   end
 end
 
@@ -26,7 +26,7 @@ describe("Words#is_anagram") do
     expect(words.is_anagram("The Morse Code", "Here come dots!")).to(eq("These words are anagrams."))
   end  
   it("returns false if word1 and word2 are not anagrams") do
-    expect(words.is_anagram("Tea", "pea")).to(eq(false))
+    expect(words.is_anagram("Tea", "pea")).to(eq("These words are neither an anagram or antigram"))
   end
 end
 
