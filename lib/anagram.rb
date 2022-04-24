2bef7ada28eca0e0130fe13e6372a76c3e2862f4
cameronjohnson30 <cameron_johnson91@icloud.com>
2022-04-23 20:44:56 -0700

    deleted redundent line of code for readability

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,7 +2,7 @@ class Words
   def is_word(word1, word2)
     if word1.scan(/[aeiou]/).count >= 1 && word2.scan(/[aeiou]/).count >= 1
       is_anagram(word1,word2)
-    else word1.scan(/[aeiou]/).count >= 1 && word2.scan(/[aeiou]/).count >= 0 || word1.scan(/[aeiou]/).count >= 0 && word2.scan(/[aeiou]/).count >= 1 || word1.scan(/[aeiou]/).count >= 0 && word2.scan(/[aeiou]/).count >= 0
+    else
       p "please check your spelling" 
     end
   end
