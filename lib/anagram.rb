b1b4f5ebfdcbb082bd858719489b7aca01c9d4af
cameronjohnson30 <cameron_johnson91@icloud.com>
2022-04-24 00:33:14 -0700

    added instance variables

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,15 +1,23 @@
 class Words
+
+  def initialize()
+    @failed = "please check your spelling" 
+    @anagram = "These words are anagrams."
+    @antigram = "These words have no letter matches and are antigrams."
+    @noMatch = "These words are neither an anagram or antigram"
+  end
+
   def is_word(word1, word2)
     if word1.scan(/[aeiou]/).count >= 1 && word2.scan(/[aeiou]/).count >= 1
       is_anagram(word1,word2)
     else
-      p "please check your spelling" 
+      p @failed
     end
   end
 
   def is_anagram(word1, word2)
     if word1.downcase.chars.sort.join.gsub(/[^a-z ]/, '') == word2.downcase.chars.sort.join.gsub(/[^a-z ]/, '')
-      p "These words are anagrams."
+      p @anagram
     else
       is_antigram(word1, word2)
     end
@@ -19,9 +27,9 @@ class Words
     a = word1.downcase.split("")
     b = word2.downcase.split("") 
     if !(a & b).empty? == true 
-      p "These words are neither an anagram or antigram"
+      p @noMatch
     else  !(a & b).empty? == false 
-      p "These words have no letter matches and are antigrams."
+      p @antigram
     end
   end
 end
